79e3b8b7a82db9b000187d570fc2a25221cd6db5
Aylin1252 <L25550699@chihuahua2.tecnm.mx>
2026-09-03 19:58:11 -0600

    Add files via upload

---
+++ EVA1_2_STACK/src/eva1_2_stack/EVA1_2_STACK.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_stack;
+
+/**
+ *
+ * @author aylin
+ */
+public class EVA1_2_STACK {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      System.out.println("INICIA main");
+          A();
+          System.out.println("TERMINA main");
+   }
+   public static void A(){
+      System.out.println("INICIA A()");
+      B();
+      System.out.println("TERMINZ A()");
+   }
+   public  static void B(){
+      System.out.println("INICIA B()");
+   }
+   int i=5;
+   double d=5.5;
+   System.out.println("TERMINA B()");
+}
